521b7b358509aa7f30bc8764d24ec89e7c8e2907
ranjeetcool <ranjeetcoolranjeet+github@gmail.com>
2023-05-05 17:52:40 +0530

    add fetch data into cards for smaller devices

--- src/Components/FetchData.js
+++ src/Components/FetchData.js
@@ -3,48 +3,81 @@ import './FetchData.css'
 
 function FetchData() {
   const [users, setUsers] = useState([])
-  
+  const [swidth, setSwidth] = useState(window.innerWidth)
+
+
   const urlEndPoint = 'https://jsonplaceholder.typicode.com/users/'
-   
+
   useEffect(() => {
     fetch(urlEndPoint)
-    .then(response => response.json())
-    .then(datas => {console.log(datas)
-      setUsers(datas)})
-      .catch(e=> {console.log('e',e)})
-  },[])
+      .then(response => response.json())
+      .then(datas => {
+        console.log(datas)
+        setUsers(datas)
+      })
+      .catch(e => { console.log('e', e) })
+
+  }, [])
+
+
+  window.addEventListener('resize',()=>{
+    const wd = window.innerWidth
+    setSwidth(wd)
+  })
 
   return (
     <>
-    <span><h1 className='text_header'> API Data Fetch</h1> </span>
-    <div className='table_data'>
-      <table >
-        <thead className='table_header'>
-        <tr >
-          <th>No.</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Address</th>
-        </tr>
-        </thead>
-        <tbody className='table_body'>
-        {users.length > 0 && (
-          users.map(user => (
-            <tr className='table_body' key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
-              <td>{user.address.street} {user.address.city} {user.address.zipcode}</td>
+      <span><h1 className='text_header'> API Data Fetch</h1> </span>
+      <div className='table_data'>
+        {swidth > 660 ? (<table >
+          <thead className='table_header'>
+            <tr >
+              <th>No.</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Address</th>
             </tr>
-          ))
+          </thead>
+          <tbody className='table_body'>
+            {users.length > 0 && (
+              users.map(user => (
+                <tr className='table_body' key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.address.street} {user.address.city} {user.address.zipcode}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+        ) : users.length > 0 && (
+          users.map(user => (
+            <div className='data_user_card' key={user.id}>
+              <div className='row1'>
+                <span>{user.id}{'.'}</span>
+                <span>{user.name}</span>
+              </div>
+              <div className='row2'>
+                <div className='text_head'>Phone</div>
+                <div className='text_data'>{user.phone}</div>
+                <div className='text_head'>Email</div>
+                <div className='text_data'>{user.email}</div>
+              </div>
+              <div className='row3'>
+                <div className='text_head'>Address</div>
+                <div className='text_data'>{user.address.street}</div>
+                <div className='text_head'>City and Zipcode</div>
+                <div className='text_data'>{user.address.city} {user.address.zipcode}</div>
+              </div>
+
+            </div>))
         )}
-        </tbody>
-      </table>
+
       </div>
 
-      {console.log(users)}
     </>
   )
 }
